9fa4166e2d3fd1bd8b0b893f8df454ac8fcc1559
Sindy Tatiana Suarez Baron <sitysb@gmail.com>
2025-08-12 11:48:18 -0500

    Solucion2

--- temperatura.js
+++ temperatura.js
@@ -6,7 +6,7 @@ let temperatura = prompt("Ingrese una temperatura en grados Celcius: ");
 
 // si es un numero y si no hay espacio vacios ejecuta
 if (!isNaN(temperatura) && temperatura.trim() != ''){ // isNan = devuelve true si no es numero
-    temperatura = parseFloat(temperatura); // recibimos el numero y luego lo pasamos a numero
+    temperatura = parseFloat(temperatura); // recibimos el string y luego lo pasamos a numero
     
     let kelvin = temperatura + 273.15;
     let fahrenheit = (temperatura * (9/5) + 32);
